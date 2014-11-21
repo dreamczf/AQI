a71c77dcc86c0eef970a3d50dd4d46e580e0326c
pandengyang <dengyang.pan@foxmail.com>
2014-11-21 17:24:00 +0800

    add config

--- include/result.php
+++ include/result.php
@@ -2,6 +2,7 @@
 define('AQI_OK', 0)
 define('AQI_PARAM_ERROR', 1)
 define('AQI_REPEAT_ACCOUNT_ID', 2)
+define('AQI_NO_CONTACT_TYPE', 3)
 
 class Result {
 	public errorcode;
--- login.php
+++ login.php
@@ -9,25 +9,25 @@ $passwd = mysqli_real_escape_string($dbc, trim(_POST['passwd']));
 if(empty($account_id) || empty($passwd)) {
 	$result->errorcode = PARAM_ERROR;
 	$result->description = 'param error';
-	aqi_exit(NULL, $result);
+	exit(NULL, $result);
 }
 
 $dbc = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
-$query = "SELECT hased_passwd from aqi_account WHERE account_id=$account_id";
+$query = "SELECT hased_passwd from account WHERE account_id=$account_id";
 $res = mysqli_query($dbc, $query);
 $row = mysqli_fetch_array($res);
 if(!$row) {
 	$result->errorcode = ACCOUNT_INFO_ERROR;
 	$result->description = 'account info error';
-	aqi_exit($dbc, $result);
+	exit($dbc, $result);
 }
 
 if(hash($passwd) != $row['hashed_passwd']) {
 	$result->errorcode = ACCOUNT_INFO_ERROR;
 	$result->description = 'account info error';
-	aqi_exit($dbc, $result);
+	exit($dbc, $result);
 }
 
 $result->errorcode = OK;
-aqi_exit($dbc, $result);
+exit($dbc, $result);
